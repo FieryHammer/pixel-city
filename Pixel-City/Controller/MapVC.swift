6041efa0ea53ad239b3d9f1f8bae8979ff116f2b
Horvath, Mate <Mate.Horvath.@misys.com>
2018-11-05 14:34:25 +0100

    🐣 FEATURE : Added elements for the pullUp view and set basics for Flickr API

--- Pixel-City/Controller/MapVC.swift
+++ Pixel-City/Controller/MapVC.swift
@@ -17,14 +17,30 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     let authorizationStatus = CLLocationManager.authorizationStatus()
     let regionRadius: Double = 1000
     
+    var pullupViewIsUp = false
+    @IBOutlet weak var pullUpViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pullUpView: UIView!
+    
+    var spinner: UIActivityIndicatorView?
+    var progressLbl: UILabel?
+    
+    var flowLayout = UICollectionViewFlowLayout()
+    var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
         locationManager.delegate = self
         configureLocationServices()
-        
         setupGestureRecognizers()
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: PHOTO_CELL_IDENTIFIER)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        pullUpView.addSubview(collectionView!)
+        collectionView?.backgroundColor = UIColor.green
     }
 
     @IBAction func centerMapBtnPressed(_ sender: Any) {
@@ -39,6 +55,73 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         doubleTapGestureRecognizer.delegate = self
         
         mapView.addGestureRecognizer(doubleTapGestureRecognizer)
+        
+        let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeGestureRecognizer.direction = .down
+        pullUpView.addGestureRecognizer(swipeGestureRecognizer)
+    }
+    
+    @objc func handleSwipe() {
+            animateViewDown()
+    }
+    
+    func animateViewUp() {
+        pullUpViewHeightConstraint.constant = 300
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+        pullupViewIsUp = true
+        
+        addSpinner()
+        addProgressLbl()
+    }
+    
+    func animateViewDown() {
+        pullUpViewHeightConstraint.constant = 0
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+        pullupViewIsUp = false
+    }
+    
+    func addSpinner() {
+        removeSpinner()
+        
+        spinner = UIActivityIndicatorView()
+        spinner?.center = CGPoint(x: (pullUpView.frame.width / 2) - ((spinner?.frame.width)! / 2), y: pullUpView.frame.height / 2)
+        spinner?.style = .whiteLarge
+        spinner?.color = UIColor.darkGray
+        spinner?.startAnimating()
+        
+        collectionView?.addSubview(spinner!)
+    }
+    
+    func removeSpinner() {
+        if spinner != nil {
+            spinner?.removeFromSuperview()
+        }
+    }
+    
+    func addProgressLbl() {
+        removeProgressLbl()
+        
+        let labelWidth: CGFloat = 240
+        
+        progressLbl = UILabel()
+        progressLbl?.frame = CGRect(x: (pullUpView.frame.width / 2) - (labelWidth / 2), y: 175, width: labelWidth, height: 40)
+        progressLbl?.font = UIFont(name: "Avenir Next", size: 18)
+        progressLbl?.textColor = UIColor.darkGray
+        progressLbl?.textAlignment = .center
+        
+        collectionView?.addSubview(progressLbl!)
+    }
+    
+    func removeProgressLbl() {
+        if progressLbl != nil {
+            progressLbl?.removeFromSuperview()
+        }
     }
     
 }
@@ -49,7 +132,7 @@ extension MapVC: MKMapViewDelegate {
             return nil
         }
         
-        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: DROPPABLE_PIN_IDENTIFIER)
         pinAnnotation.pinTintColor = #colorLiteral(red: 0.9771530032, green: 0.7062081099, blue: 0.1748393774, alpha: 1)
         pinAnnotation.animatesDrop = true
         
@@ -67,12 +150,15 @@ extension MapVC: MKMapViewDelegate {
     @objc func dropPin(gestureRecognizer: UITapGestureRecognizer) {
         let touchPoint = gestureRecognizer.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
-        let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
+        let annotation = DroppablePin(coordinate: touchCoordinate, identifier: DROPPABLE_PIN_IDENTIFIER)
         
         removePins()
         mapView.addAnnotation(annotation)
         let coordinateRegion = MKCoordinateRegion(center: touchCoordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
+        animateViewUp()
+        
+        print(flickrURL(apiKey: FLICKR_API_KEY, annotation: annotation, numberOfPhotos: 40))
     }
     
     func removePins() {
@@ -89,5 +175,20 @@ extension MapVC: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         centerMapOnUserLocation()
+    }
+}
+
+extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //number of items in array
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PHOTO_CELL_IDENTIFIER, for: indexPath) as? PhotoCell {
+                return cell
+        }
+        
+        return PhotoCell()
     }
 }
---
+++ Pixel-City/View/PhotoCell.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoCell.swift
+//  Pixel-City
+//
+//  Created by Horvath, Mate on 2018. 11. 05..
+//  Copyright © 2018. Finastra. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
